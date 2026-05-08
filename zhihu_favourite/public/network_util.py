3f2c0553f6bcdeb95a536a7bba1a01e84f7c2c7f
ainfkcn <ainfkcn@outlook.com>
2026-05-08 19:32:36 +0800

    refactor: 元数据获取提取至公共函数

--- zhihu_favourite/public/network_util.py
+++ zhihu_favourite/public/network_util.py
@@ -1,6 +1,8 @@
 import re
+import json
 
 from loguru import logger
+from datetime import datetime
 from playwright.sync_api import sync_playwright
 
 
@@ -116,3 +118,98 @@ def get_json_str(answer):
             return None
         finally:
             browser.close()
+
+def update_metadata(df, index):
+    answer = df.loc[index]["answer"]
+    title = df.loc[index]["title"]
+
+    json_str = get_json_str(answer)
+    if not json_str:
+        return
+    try:
+        json_data = json.loads(json_str)
+    except json.JSONDecodeError:
+        logger.error(f"JSON解析失败，跳过当前文件")
+        return
+
+    # 回答
+    entities = json_data["initialState"]["entities"]
+    if entities["answers"]:
+        answers = dict(list(entities["answers"].values())[0])
+
+        created_time = datetime.fromtimestamp(answers["createdTime"]).strftime(
+            "%Y-%m-%d"
+        )
+        edited_time = datetime.fromtimestamp(answers["updatedTime"]).strftime(
+            "%Y-%m-%d"
+        )
+        favorite_time_after = created_time
+        author = answers["author"]["name"]
+        author_id = answers["author"]["id"]
+    # 专栏
+    elif entities["users"] and entities["articles"]:
+        users = dict(list(entities["users"].values())[0])
+        articles = dict(list(entities["articles"].values())[0])
+
+        created_time = datetime.fromtimestamp(articles["created"]).strftime("%Y-%m-%d")
+        edited_time = datetime.fromtimestamp(articles["updated"]).strftime("%Y-%m-%d")
+        favorite_time_after = created_time
+        author = articles["author"]["name"]
+        author_id = articles["author"]["id"]
+    # 想法——知乎动态
+    elif entities["users"] and entities["pins"]:
+        users = dict(list(entities["users"].values())[0])
+        pins = dict(list(entities["pins"].values())[0])
+
+        created_time = datetime.fromtimestamp(pins["created"]).strftime("%Y-%m-%d")
+        edited_time = datetime.fromtimestamp(pins["updated"]).strftime("%Y-%m-%d")
+        favorite_time_after = created_time
+        author = users["name"]
+        author_id = users["id"]
+        if "[object Object]" in df.loc[index]["answer"]:
+            answer = re.sub(
+                r"\[object Object\](,\[object Object\])*",
+                pins["content"][0]["content"],
+                answer,
+            )
+            if pins["content"][0]["title"]:
+                title = pins["content"][0]["title"]
+            for pic in pins["content"][1:]:
+                try:
+                    answer += f"\n\n![image]({pic['originalUrl']})"
+                except:
+                    continue
+    # 视频 视频内容需要手动下载，此处只能解决元数据
+    elif entities["users"] and entities["zvideos"]:
+        users = dict(list(entities["users"].values())[0])
+        zvideos = dict(list(entities["zvideos"].values())[0])
+
+        created_time = datetime.fromtimestamp(zvideos["publishedAt"]).strftime(
+            "%Y-%m-%d"
+        )
+        edited_time = datetime.fromtimestamp(zvideos["updatedAt"]).strftime("%Y-%m-%d")
+        favorite_time_after = created_time
+        author = users["name"]
+        author_id = users["id"]
+    # 和谐
+    else:
+        logger.error(f"已经和谐了，无法获取元数据")
+        df.at[index, "censored"] = True
+        df.at[index, "modified"] = True
+        return
+
+    df.at[index, "created_time"] = created_time
+    df.at[index, "edited_time"] = edited_time
+    df.at[index, "favorite_time_after"] = favorite_time_after
+    # 这里为了方便考虑：未清洗数据作者名必然变化；清洗后数据只有作者名会变化
+    if author != df.loc[index]["author"]:
+        logger.info(f"作者改名: {df.loc[index]['author']} -> {author}")
+        df.at[index, "modified"] = True
+    df.at[index, "author"] = author
+    df.at[index, "author_id"] = author_id
+    df.at[index, "title"] = title
+    df.at[index, "answer"] = answer
+
+    logger.info(
+        f"获取元数据成功: 作者: {author}, 创建时间: {created_time}, 编辑时间: {edited_time}"
+    )
